9ae34e6bd537b7f14f7e6bdc139268f8519a1eeb
Tanmay Madaan <tanmaymadaan@outlook.com>
2020-07-04 11:20:13 +0530

    UI changes to HomeActivity

--- app/src/main/java/com/tanmaymadaan/swypr/activities/HomeActivity.java
+++ app/src/main/java/com/tanmaymadaan/swypr/activities/HomeActivity.java
@@ -20,6 +20,25 @@ public class HomeActivity extends AppCompatActivity {
         View view = mBinding.getRoot();
         setContentView(view);
 
+        //Fetch and display name
+        String welcomeMsg = getString(R.string.welcome) + " Tanmay!";
+        mBinding.welcomeTv.setText(welcomeMsg);
+
+        //Fetch and display distance travelled
+        String distTravelled = getString(R.string.dist_trav) + " 20KM";
+        mBinding.distanceTv.setText(distTravelled);
+
+        //Fetch and display swipe-in time
+        String swipeInTime = getString(R.string.swipeIn) + " 10:27AM";
+        mBinding.swipeTimeTv.setText(swipeInTime);
+
+        //Fetch and display total time
+        String totalTime = getString(R.string.totalTime) + " 3hr48min";
+        mBinding.totalTimeTv.setText(totalTime);
+
+        //Fetch and display current check-in
+        String checkIn = getString(R.string.currCheckIn) + " CMC";
+        mBinding.currCheckinTv.setText(checkIn);
 
     }
 }
